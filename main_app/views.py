c0de380159f0626442c79606936aa111cb4200fb
ManliestBen <benjamintmanley@gmail.com>
2019-09-13 10:32:16 -0500

    add detail view

--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,7 @@ def projects_index(request):
     projects = Project.objects.all()
     return render(request, 'projects/index.html', { 'projects': projects })
 # Create your views here.
+
+def projects_detail(request, project_id):
+  project = Project.objects.get(id=project_id)
+  return render(request, 'projects/detail.html', { 'project': project })
