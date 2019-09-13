7a94461ce47c0875c7cf0d8c4137f048cf40240a
ManliestBen <benjamintmanley@gmail.com>
2019-09-12 23:38:21 -0500

    add project cards

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    hardware = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    language = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
 def home(request):
-    return HttpResponse('<h1>Plan Your Project!!!!</h1>')
+    return render(request, 'projects/index.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def projects_index(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/index.html', { 'projects': projects })
 # Create your views here.
