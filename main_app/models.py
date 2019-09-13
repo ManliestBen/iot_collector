f7f9b5245e95ccce411ffcad1aff58e439e8753a
ManliestBen <benjamintmanley@gmail.com>
2019-09-13 12:12:16 -0500

    add progress view

--- main_app/models.py
+++ main_app/models.py
@@ -5,9 +5,18 @@ class Project(models.Model):
     hardware = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     language = models.CharField(max_length=100)
+    
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'project_id': self.id})
+
+class Progress(models.Model):
+    date = models.DateField('progress date')
+    note = models.CharField(max_length=100)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.note} on {self.date}"
